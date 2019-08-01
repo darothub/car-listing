b1192c7da95aecc93399f8ecfbad1ba4257920eb
darothub <darotudeen@gmail.com>
2019-08-01 20:18:46 +0100

    add jquery lib to register

--- js/register.js
+++ js/register.js
@@ -1,34 +1,34 @@
 $(document).ready(function(){
-    console.log('hey')
+    
     $('form').submit(function(event){
+        
          
         var formData = {
-            'firstName' : $('input[id=first_name]').val(),
-            'lastName' : $('input[id=last_name]').val(),
-            'email' : $('input[type=email]').val(),
-            'password' :  $('input[type=password]').val(),
-            'password2' : $('#exampleInputPassword2').val()
-    
+            'firstName' : $('input[name=first]').val(),
+            'lastName' : $('input[name=last]').val(),
+            'email' : $('input[name=email]').val(),
+            'password' : $('input[name=password1]').val(),
+            'password2' : $('input[name=cpassword]').val(),
         };
         if(formData.password !== formData.password2){
             
             alert('password does not match')
             return false
         }
-    
+        console.log(formData)
         $.ajax({
             type    : 'POST',
             url     : `https://car-flux.herokuapp.com/users`,
             data    : formData,
             dataType : 'json',
             encode   : true
-        })
-        .done(function(data){
+        }).done(function(data){
 
-            $('.alert-success').css({display: "block"}).fadeIn(3000)
+          
             $('.alert-success').show();
             alert('proceed to login')
-            window.location.assign('login.html');               
+            window.location.assign('login.html').delay(2000)
+                           
                     
                            
        })
