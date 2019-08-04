431706b9ccc33f6df7c1719b6a6862e4b5fe873b
darothub <darotudeen@gmail.com>
2019-08-04 23:09:24 +0100

    Add features to avoid duplicate users

--- js/register.js
+++ js/register.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
     
     $('form').submit(function(event){
-        
-         
+
         var formData = {
             'firstName' : $('input[name=first]').val(),
             'lastName' : $('input[name=last]').val(),
@@ -10,33 +9,44 @@ $(document).ready(function(){
             'password' : $('input[name=password1]').val(),
             'password2' : $('input[name=cpassword]').val(),
         };
+
         if(formData.password !== formData.password2){
             
             alert('password does not match')
             return false
         }
-       
-        $.ajax({
-            type    : 'POST',
-            url     : ` https://car-flux.herokuapp.com/users`,
-            data    : formData,
-            dataType : 'json',
-            encode   : true
-        }).done(function(data){
 
-          
-            $('.alert-success').show();
-            alert('proceed to login')
-            window.location.assign('login.html').delay(2000)
-                           
-                    
-                           
-       })
-        .fail(function(){
-            alert('oops! server error')
-        })
-        
-        event.preventDefault();
+        $.getJSON('https://car-flux.herokuapp.com/users', function(data){
+            var existingUser = data.find(item => item.email === formData['email'] )
+            console.log(existingUser)
+            if(existingUser){
+                alert('email has been used')
+                return false
+            }
+       
+           
+            $.ajax({
+                type    : 'POST',
+                url     : ` https://car-flux.herokuapp.com/users`,
+                data    : formData,
+                dataType : 'json',
+                encode   : true
+            }).done(function(data){
     
+              
+                $('.alert-success').show();
+                alert('proceed to login')
+                window.location.assign('login.html').delay(2000)
+                               
+                        
+                               
+           })
+            .fail(function(){
+                alert('oops! server error')
+            })
+            
+        })
+        event.preventDefault()
     })
+        
 })
